ef1ab37cac7016e914d0c149e7bf365cc31d75e8
yyutter <2546868931@qq.com>
2016-10-08 10:43:15 +0800

    增加后台自动更新天气的功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -53,12 +53,15 @@ public class ChooseAreaActivity extends Activity{
 	
 	private int currentLevel;
 	
+	private boolean isFromWeatherActivity;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if (prefs.getBoolean("city_selected", false)){
+		if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity){
 			Intent intent = new Intent(this, WeatherActivity.class);
 			startActivity(intent);
 			finish();
@@ -210,6 +213,10 @@ public class ChooseAreaActivity extends Activity{
 		}else if (currentLevel == LEVEL_CITY){
 			queryProvinces();
 		}else{
+			if (isFromWeatherActivity){
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+			}
 			 finish();
 		}
 	}
---
+++ src/com/coolweather/app/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,5 @@
+package com.coolweather.app.receiver;
+
+public class AutoUpdateReceiver {
+
+}
